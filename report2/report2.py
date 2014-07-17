8a52972a99534d2e2739a6b22fedb55c6d16a669
tsukuyomip <ab0312uo@gmail.com>
2014-07-17 14:51:11 +0900

    MyGraphのプロトタイプを作成
    !! phi_1 と phi_2の結果がほぼ同じ（45.0前後）である．
    恐らくphi_1関連の処理が間違えているため，要確認．

--- report2/report2.py
+++ report2/report2.py
@@ -1,4 +1,20 @@
 #coding: utf-8
 
+import sys
+import numpy as np
+import MyGraph as mg
+
 if __name__ == "__main__":
-    print "hello"
+    N_EXP = 2000
+    start = 3
+    goal = 6
+    rng = np.random.RandomState(22294322)
+
+    g = mg.MyGraph(rng = rng)
+
+    sum_step = 0
+    for i in xrange(N_EXP):
+        sum_step += g.walk(start = start, goal = goal)
+        print >> sys.stderr, i, "/", N_EXP
+
+    print float(sum_step)/N_EXP
